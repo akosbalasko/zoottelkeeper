7b3429f92ed02f98def8bfa6471463c29e4e9e0c
Akos Balasko <akos0215@gmail.com>
2020-07-11 08:49:51 +0200

    os-free newline characters

--- src/updateIndexFile.ts
+++ src/updateIndexFile.ts
@@ -1,11 +1,12 @@
 import path = require('path');
 import * as fs from 'fs';
+import { EOL } from 'os';
 
 export const updateIndexFile = (fileFolder: string): void => {
     const folderName = path.basename(fileFolder);
     const folderEntries = fs.readdirSync(fileFolder);
 
     const filesNames = folderEntries.filter(dirent => fs.lstatSync(path.join(fileFolder, dirent)).isFile());
-    const indexContent = filesNames.map(fileName => `[[${fileName}]]`).join('\n');
+    const indexContent = filesNames.map(fileName => `[[${fileName}]]`).join(EOL);
     fs.writeFileSync(path.join(fileFolder, `000_Index_of_${folderName}.md`), indexContent);
 };
