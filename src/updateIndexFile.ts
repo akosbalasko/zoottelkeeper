a75e76fefe797e74e1c84a32c94a1b87821f1982
Akos Balasko <akos0215@gmail.com>
2020-07-12 00:02:28 +0200

    excluding hidden files and folders

--- src/updateIndexFile.ts
+++ src/updateIndexFile.ts
@@ -6,10 +6,11 @@ export const updateIndexFile = (fileFolder: string): void => {
     const targetFolderName = path.basename(fileFolder);
     const folderEntries = fs.readdirSync(fileFolder);
 
-    const folderNames = folderEntries.filter(dirent => fs.lstatSync(path.join(fileFolder, dirent)).isDirectory());
+    const folderNames = folderEntries.filter(
+        dirent => fs.lstatSync(path.join(fileFolder, dirent)).isDirectory() && !dirent.startsWith('.'));
     let indexContent = folderNames.map(folderName => `[[000_Index_of_${folderName}]]`).join(EOL) + EOL;
 
-    const filesNames = folderEntries.filter(dirent => fs.lstatSync(path.join(fileFolder, dirent)).isFile());
+    const filesNames = folderEntries.filter(dirent => fs.lstatSync(path.join(fileFolder, dirent)).isFile() && !dirent.startsWith('.'));
     indexContent += filesNames.map(fileName => `[[${fileName}]]`).join(EOL);
     fs.writeFileSync(path.join(fileFolder, `000_Index_of_${targetFolderName}.md`), indexContent);
 };
